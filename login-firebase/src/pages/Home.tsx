cf8e806e569debe73b6c397e20a5cfb830dd76ae
Leonardo Lima <leonardo1230987@gmail.com>
2021-07-23 02:43:33 -0400

    Página de signIn e de signUp estilizadas. falta responsividade.

--- login-firebase/src/pages/Home.tsx
+++ login-firebase/src/pages/Home.tsx
@@ -2,13 +2,16 @@ import { useState, FormEvent, useContext } from "react"
 import { useHistory } from 'react-router-dom'
 
 import { AuthContext } from "../contexts/AuthContext"
-import loginImg from '../assets/images/login.svg'
+
+import unlockImg from '../assets/images/unlock.svg'
+
+import '../styles/home.scss'
 
 export function Home() {
   const history = useHistory()
 
-  const { user, createUser, signIn, signOut } = useContext(AuthContext)
-  
+  const { user, createUser, signIn } = useContext(AuthContext)
+
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [switchTab, setSwitchTab] = useState(false)
@@ -33,66 +36,111 @@ export function Home() {
       await createUser(email, password)
       history.push('/welcome')
     }
-  } 
-
-  async function handleSignOut() {
-    if (user) {
-      await signOut()
-    }
   }
 
   return (
-    <div id="page-auth">
+    <div id="home">
       <aside>
-        <img src={loginImg} alt="Login" />
-        <h1>Entre com e-mail e senha.</h1>
+        <div className="text">
+          <h1>Lorem ipsum dolor sit amet.</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Unde praesentium beatae architecto distinctio omnis illo ab!</p>
+        </div>
+        <img src={unlockImg} alt="Login" />
       </aside>
       <main>
         <div className="main-content">
-          <div className="tabs">
-            <button onClick={handleSwitchTab}>Entrar</button>
-            <button onClick={handleSwitchTab}>Registrar</button>
-          </div>
-          <div className={`singIn ${switchTab ? 'invisible' : ''}`}>
-            <form onSubmit={handleLogin}>
-              <input
-                type="text"
-                placeholder="E-mail"
-                spellCheck={false}
-                onChange={event => setEmail(event.target.value)}
-                value={email}
-              />
-              <input
-                type="password"
-                placeholder="Senha"
-                spellCheck={false}
-                onChange={event => setPassword(event.target.value)}
-                value={password}
-              />
-              <button type="submit">Entrar</button>
+          <h2 className={`${switchTab ? 'invisible' : ''}`}>
+            Sign in to Login React
+          </h2>
+          <h2 className={`${!switchTab ? 'invisible' : ''}`}>
+            Sign up to Login React
+          </h2>
+          <div className="form-container">
+            <div className="tabs">
+              <button
+                className={`tab ${!switchTab ? 'active' : ''}`}
+                onClick={handleSwitchTab}
+              >
+                Sign In
+              </button>
+              <button
+                className={`tab ${switchTab ? 'active' : ''}`}
+                onClick={handleSwitchTab}
+              >
+                Sign Up
+              </button>
+            </div>
+            <form
+              className={`form ${switchTab ? 'invisible' : ''}`}
+              onSubmit={handleLogin}
+            >
+              <fieldset>
+                <label htmlFor="email">Email</label>
+                <input
+                  id="email"
+                  type="text"
+                  placeholder="example@mail.com"
+                  spellCheck={false}
+                  onChange={event => setEmail(event.target.value)}
+                  value={email}
+                />
+              </fieldset>
+              <fieldset>
+                <label htmlFor="password">Password</label>
+                <input
+                  id="password"
+                  type="password"
+                  placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
+                  spellCheck={false}
+                  onChange={event => setPassword(event.target.value)}
+                  value={password}
+                />
+              </fieldset>
+              <button type="submit">Sign In</button>
+              <p>
+                Don't have an account yet?&nbsp;
+                <span onClick={handleSwitchTab}>
+                  Sign up
+                </span>
+              </p>
             </form>
-          </div>
-          <div className={`createUser ${!switchTab ? 'invisible' : ''}`}>
-            <form onSubmit={handleCreateUser}>
-              <input
-                type="text"
-                placeholder="E-mail"
-                spellCheck={false}
-                onChange={event => setEmail(event.target.value)}
-                value={email}
-              />
-              <input
-                type="password"
-                placeholder="Senha"
-                spellCheck={false}
-                onChange={event => setPassword(event.target.value)}
-                value={password}
-              />
-              <button type="submit">Criar Usuário</button>
+            <form
+              className={`form ${!switchTab ? 'invisible' : ''}`}
+              onSubmit={handleCreateUser}
+            >
+              <fieldset>
+                <label htmlFor="email2">Email</label>
+                <input
+                  id="email2"
+                  type="text"
+                  placeholder="example@mail.com"
+                  spellCheck={false}
+                  onChange={event => setEmail(event.target.value)}
+                  value={email}
+                />
+              </fieldset>
+              <fieldset>
+                <label htmlFor="password2">Password</label>
+                <input
+                  id="password2"
+                  type="password"
+                  placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
+                  spellCheck={false}
+                  onChange={event => setPassword(event.target.value)}
+                  value={password}
+                />
+              </fieldset>
+              <button type="submit">Create an account</button>
+              <p>
+              Already have an account?&nbsp;
+                <span onClick={handleSwitchTab}>
+                  Sign in
+                </span>
+              </p>
             </form>
           </div>
         </div>
-        <button onClick={handleSignOut}>Logout</button>
       </main>
     </div>
   )
